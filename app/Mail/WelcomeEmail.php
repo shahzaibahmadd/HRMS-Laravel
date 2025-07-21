63ab89f36a9e496e01133ba8a90b4d32f7ddcfdf
KhubaibShabbir4 <f219634@cfd.nu.edu.pk>
2025-07-21 15:44:15 +0500

    mail work done

---
+++ app/Mail/WelcomeEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class WelcomeEmail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $logoCid;
+
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    public function build()
+    {
+        $logoUrl = asset('logo.jpg'); // Ensure logo.jpg is in public/
+
+        \Log::info("Building Welcome Email for user: {$this->user->email}");
+
+        return $this->subject('Welcome to HRMS of DEVELOPERS Studio!')
+            ->view('emails.welcome')
+            ->with([
+                'name'  => $this->user->name,
+                'email' => $this->user->
+
+                email,
+                'logo'  => $logoUrl
+            ]);
+    }
+
+}
